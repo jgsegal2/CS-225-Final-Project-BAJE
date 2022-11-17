4b49983b5d6eac1200925a400c1644019d2a01dc
jgsegal2 <97977852+jgsegal2@users.noreply.github.com>
2022-11-16 20:57:16 -0600

    Update data_parsing.cpp
    Finished basic structure of data parsing. Need to talk to Benson to change calculate weights function and also may need to change weights in graph to double. Also may need to add 1 to each .at() for the vertices.

--- data_parsing.cpp
+++ data_parsing.cpp
@@ -2,34 +2,31 @@
 #include <string>
 
 void read_in_file(){
+  //vertexes
   std::ifstream file(/../airports.dat);
   for(std::string line; std::getline(file, line); line=""){
     //int comma_count= 0;
-    std::string location;
-    std::string latitude;
-    std:string longitude;
     std::vector <std::string> vect;
-    
-    for(int i=0; i<line.size(); i++){
-      char on= line.at(i);
-      if (on==','){
-        comma_count++;
-      }else{
-        //if country is US
-      //if(comma_count == // location number){
-      // location.push_back(on)
-      //}
-      //if(comma_count == // lat number){
-      // latitude.push_back(on)
-      //}
-      //if(comma_count == // long number){
-      // longitude.push_back(on)
-      //}
-      }
+    SplitString(line, ',', vect);
+    int SplitString(const std::string & str1, char sep, std::vector<std::string> &fields);
+    if (vect.at(2).compare("United States")==0 && vect.at(3).compare("") != 0){
+      g_.insertVertex(Vertex(vect.at(3)));
+      coords[vect.at(3)] = std::pair<double, double> (std::stod(vect.at(5)), std::stod(vect.at(6)));
     }
 
-    double lat_d=std::stod(latitude);
-    double long_d=std::stod(longitude);
-    //need to figure out where to store these (variable) //want to use graph from lab with slight modifications (need to talk to team)
+  }
+  //edges
+  std::ifstream file2(/../routes.dat);
+  for(std::string line; std::getline(file2, line); line=""){
+    std::vector <std::string> vect;
+    SplitString(line, ',', vect);
+    Vertex orgin(vect.at(2);
+    Vertex dest(vect.at(4));
+    if(vertexExists(orgin) && vertexExists(dest)){
+      g_.insertEdge(orgin, dest);
+      double weight= calculate_weight(orgin, dest); //need to ask Benson to change function, seems most efficient to do this here in the initialization
+      g_.setEdgeWeight(orgin, dest, weight);
+      
+    }
   }
 }
