79b6addd7927bf4a798c73b9ef19d8f8923ccc9f
benson-b <80379380+benson-b@users.noreply.github.com>
2022-11-18 11:56:18 -0600

    Update FlightFinder.cpp

--- src/FlightFinder.cpp
+++ src/FlightFinder.cpp
@@ -12,9 +12,9 @@ void FlightFinder::read_in_file(){
     std::vector <std::string> vect;
     SplitString(line, ',', vect);
     int SplitString(const std::string & str1, char sep, std::vector<std::string> &fields);
-    if (vect.at(2).compare("United States")==0 && vect.at(3).compare("") != 0){
-      g_.insertVertex(Vertex(vect.at(3)));
-      coords[vect.at(3)] = std::pair<double, double> (std::stod(vect.at(5)), std::stod(vect.at(6)));
+    if (vect.at(3).compare("United States")==0 && vect.at(4).compare("") != 0){
+      g_.insertVertex(Vertex(vect.at(4)));
+      coords[vect.at(4)] = std::pair<double, double> (std::stod(vect.at(6)), std::stod(vect.at(7)));
     }
 
   }
