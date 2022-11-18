e8929e6eb63c9422e99baad4e11931985861f746
jgsegal2 <97977852+jgsegal2@users.noreply.github.com>
2022-11-18 12:03:29 -0600

    Update FlightFinder.h

--- src/FlightFinder.h
+++ src/FlightFinder.h
@@ -13,7 +13,7 @@ class FlightFinder{
     //@return bool if the two places (vertices) are connected
     bool BFS(std::string orgin, std::string destination);
     //@return 5 closest lower distances and 5 closest huigher distances based on input
-    std::vector<std::string> floyl_warshall(std::string orgin, double distance);
+    std::vector<std::string> floyd_warshall(std::string orgin, double distance);
     //@return a vector containing the path of airports traveled to along with the distance attached as the last component of the vector
     std::vector <std::string> a_star(std::string orgin, std::string destination);
     
