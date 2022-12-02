87f043f388358f1bae3cbcd425c50383b8f243f8
benson-b <80379380+benson-b@users.noreply.github.com>
2022-12-01 22:55:28 -0600

    Update main.cpp
    updated main

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
 #include "FlightFinder.h"
 
 int main() {
+    FlightFinder obj;
+    obj.read_in_file();
+    std::cout << obj.BFS("LAX","CMI") << std::endl;
+    std::vector<std::string> astar_result = obj.a_star("LAX", "CMI");
+    for (unsigned i = 0; i < astar_result.size(); i++) {
+        std::cout << astar_result[i];
+        if (i < astar_result.size() - 1) {
+            std::cout << " -> ";
+        }
+    }
+    
     return -1;
 }
